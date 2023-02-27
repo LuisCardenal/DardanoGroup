c34c7d5732f4ba5324c2a56b5dea4d5a6821783c
LuisCardenal <91491173+LuisCardenal@users.noreply.github.com>
2023-02-27 14:30:04 -0600

    [FIX] Arreglar init en modelos de l10n_sv

--- l10n_sv/models/__init__.py
+++ l10n_sv/models/__init__.py
@@ -4,6 +4,5 @@ from . import account_move_sv
 from . import product_template_sv
 from . import account_journal_sv
 from . import sale_order_sv
-from . import stock_move_line_sv
-# from . import ee_sale_margin
+
 
